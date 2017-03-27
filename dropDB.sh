5c70f091279c532d3e770af404f2e5c4e2ad3c35
1anderson <anderson.allan.sobral@gmail.com>
2017-03-27 18:00:00 -0300

    resolvendo problema de consulta na query

--- dropDB.sh
+++ dropDB.sh
@@ -30,7 +30,9 @@ BEGIN
 CREATE TEMPORARY TABLE snapshot_atual SELECT * from terms where fk_snapshot_git_date=data_atual;
 CREATE TEMPORARY TABLE snapshot_anterior SELECT * from terms where fk_snapshot_git_date=data_anterior;
 insert into nascidos select null,name,fk_snapshot_git_date from snapshot_atual where name NOT IN (select name from snapshot_anterior);
-insert into falescidos select null,name,fk_snapshot_git_date from snapshot_anterior where name NOT IN (select name from snapshot_atual);
+CREATE TEMPORARY TABLE t1 select name,fk_snapshot_git_date from snapshot_anterior where name  NOT IN (select name from snapshot_atual);
+update t1 SET fk_snapshot_git_date=data_atual;
+insert into falescidos select null,name,fk_snapshot_git_date from t1;
 insert into top_terms select null,name,quantity,fk_snapshot_git_date from terms where fk_snapshot_git_date=data_atual ORDER BY quantity DESC LIMIT 10;
 END $$ 
 DELIMITER ;
