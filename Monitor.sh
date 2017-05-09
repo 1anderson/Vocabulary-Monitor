7c1edbd851a0a5feb8fafe2beee364826debb69f
1anderson <anderson.allan.sobral@gmail.com>
2017-05-09 09:40:21 -0300

    mudando formato das datas para ISO 8601

--- Monitor.sh
+++ Monitor.sh
@@ -8,19 +8,24 @@ if ! [ -e "$NODE_NAME" ] ; then
 mkdir $NODE_NAME
 fi
 
-if ! [ -e $WORKSPACE/$NODE_NAME/date.txt ]; then
-  $(date +%d%m%y > $WORKSPACE/$NODE_NAME/date.txt)    
+if ! [ -e $WORKSPACE/$NODE_NAME/dateActual.txt ]; then
+  $(date +%Y%m%d > $WORKSPACE/$NODE_NAME/dateActual.txt)
+  $(date +%Y%m%d > $WORKSPACE/$NODE_NAME/dateAfter.txt)    
 fi 
 
-date=$(cat $WORKSPACE/$NODE_NAME/date.txt)
+dateActual=$(cat $WORKSPACE/$NODE_NAME/dateActual.txt)
+dateAfter=$(cat $WORKSPACE/$NODE_NAME/dateAfter.txt)
 
-if [ $(date +%d%m%y) -gt $date ]; then
-    java -jar $path/VocabularyExtractor.jar -d . -loc iah -vxl $WORKSPACE/$NODE_NAME/Vocabulary.vxl -csv $WORKSPACE/$NODE_NAME/Vocabulary.csv -mth
+if [ $(date +%Y%m%d) -gt $dateActual ]; then
+#dateActualInIsoFormat="${dateActual:4:8}-${dateActual:2:2}-${dateActual:0:2}"
+#dateAfterInIsoFormat=$(echo "${dateAfter:4:8}-${dateAfter:2:2}-${dateAfter:0:2}")
+java -jar $path/VocabularyExtractor.jar -d . -loc iah -vxl $WORKSPACE/$NODE_NAME/Vocabulary.vxl -csv $WORKSPACE/$NODE_NAME/Vocabulary.csv -mth
 java -jar $path/TermsCounter.jar -prop ~/Documentos/Vocabulary-Monitor/termsCounter.properties -vxl $WORKSPACE/$NODE_NAME/Vocabulary.vxl -csv $WORKSPACE/$NODE_NAME/csv_result_file.csv -txt $WORKSPACE/$NODE_NAME/txt_result_file.txt -db $WORKSPACE/$NODE_NAME/terms.csv $WORKSPACE/$NODE_NAME/entities.csv $WORKSPACE/$NODE_NAME/termsXentities.csv
-$path/WriterDataBase.sh $user $password $database $WORKSPACE/$NODE_NAME/ $(date +%Y/%m/%d) $GIT_COMMIT
-$(date +%d%m%y > $WORKSPACE/$NODE_NAME/date.txt)
+$path/WriterDataBase.sh $user $password $database $WORKSPACE/$NODE_NAME/ $dateActual $dateAfter ""
+$(date +%Y%m%d> $WORKSPACE/$NODE_NAME/dateActual.txt)
+ echo "$dateActual" > $WORKSPACE/$NODE_NAME/dateAfter.txt
   else
-     $(date +%d%m%y > $WORKSPACE/$NODE_NAME/date.txt)
+     $(date +%Y%m%d > $WORKSPACE/$NODE_NAME/dateActual.txt)
 	
  fi
 
