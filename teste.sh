c5de712218a83dda7f522b1ffc70fbe0fb65d6fe
1anderson <anderson.allan.sobral@gmail.com>
2017-09-25 22:41:10 -0300

    retirando caracteres não aceito pelo SGDB

--- teste.sh
+++ teste.sh
@@ -3,7 +3,7 @@
 user=$1
 password=$2
 listOfUrls=$3
-
+a="";
 function downloadingProject(){
     git clone $1;
 }
@@ -14,12 +14,14 @@ while read -r line || [[ -n $line ]];
 do
   url=$line 
   databaseName=$(echo $line | awk -v FS="(./|.git)" '{print $(NF-1)}')
-  echo $databaseName
-  downloadingProject $url
-  ./Initializer.sh $user $password $databaseName
-  ./ReviewingHistory.sh $user $password $databaseName $databaseName
+  #echo $databaseName
+  echo $databaseName | awk '{gsub(/-/,"_",$databaseName);print $databaseName}'
+  #echo $databaseName
+  #downloadingProject $url
+  #./Initializer.sh $user $password $databaseName
+  #./ReviewingHistory.sh $user $password $databaseName $databaseName
 
-  rm -rf $databaseName
+  #rm -rf $databaseName
   
 done < url.txt
 
