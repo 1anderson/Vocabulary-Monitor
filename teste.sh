1dadbec6678a115ecccad84626ea0683ef3dc8f0
1anderson <anderson.allan.sobral@gmail.com>
2017-09-26 21:43:12 -0300

    normalizando nome para inserção no banco de dados

--- teste.sh
+++ teste.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-user=$1
-password=$2
-listOfUrls=$3
-a="";
 function downloadingProject(){
     git clone $1;
 }
 
+user=$1
+password=$2
+listOfUrls=$3
 databaseName=''
 url=''
+repositoryName=''
 while read -r line || [[ -n $line ]];
 do
   url=$line 
   databaseName=$(echo $line | awk -v FS="(./|.git)" '{print $(NF-1)}')
+  repositoryName=$databaseName;
   #echo $databaseName
-  echo $databaseName | awk '{gsub(/-/,"_",$databaseName);print $databaseName}'
-  #echo $databaseName
+  databaseName=$(echo $databaseName | awk '{gsub(/-/,"_",$databaseName); print $databaseName}')
   #downloadingProject $url
-  #./Initializer.sh $user $password $databaseName
-  #./ReviewingHistory.sh $user $password $databaseName $databaseName
+  ./Initializer.sh $user $password $databaseName
+  ./ReviewingHistory.sh $user $password $databaseName $repositoryName
 
   #rm -rf $databaseName
   
